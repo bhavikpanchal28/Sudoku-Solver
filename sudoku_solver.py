4ada2e123d3a0df1b5e6aef8d9c884c351df24db
Bhavik Panchal <32439746+bhavikpanchal28@users.noreply.github.com>
2023-10-24 02:05:50 -0400

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,90 @@
+
+def find_next_empty(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == 0:
+                return r, c
+    return None, None
+
+def solve_sudoku(puzzle):
+    row, col = find_next_empty(puzzle)
+    if row is None:
+        return True
+    for i in range(1, 10):
+        if is_valid(puzzle, i, (row, col)):
+            puzzle[row][col] = i
+            if solve_sudoku(puzzle):
+                return True
+            puzzle[row][col] = 0
+    return False
+
+
+def is_valid(puzzle, num, pos):
+    # Check row
+    for i in range(len(puzzle[0])):
+        if puzzle[pos[0]][i] == num and pos[1] != i:
+            return False
+        
+    # Check column
+    for i in range(len(puzzle)):
+        if puzzle[i][pos[1]] == num and pos[0] != i:
+            return False
+        
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+
+        for j in range(box_x * 3, box_x*3 + 3):
+            if puzzle[i][j] == num and (i,j) != pos:
+                return False    
+            
+    return True
+
+
+if __name__ == "__main__":
+    
+    #enter your puzzle here
+
+    puzzle = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]   
+
+    puzzle2 = [
+        
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+        
+    ]
+
+    solve_sudoku(puzzle)
+    solve_sudoku(puzzle2)
+
+    #print puzzle so that it looks like the above
+    for row in puzzle:
+        print(row)
+
+    print("")
+    print("puzzle 2")
+    for row in puzzle2:
+        print(row)
+
+
+
+
